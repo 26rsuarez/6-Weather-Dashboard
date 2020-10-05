537502aa22607153caa961032fa83a410526c04b
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-10-04 21:59:23 -0500

    added a function to show the weather information on the page

--- logic.js
+++ logic.js
@@ -1,12 +1,13 @@
 //array to hold the cities
 var cityArr = [];
 
-//event listener to get city from search bar
+//event listener to get city from search bar and render information
 $("#searchbtn").on("click", function(event){
     event.preventDefault();
     var city = $("#searchtext").val().trim();
     cityArr.push(city);
-    renderButtons()
+    renderButtons();
+    showWeather(city);
 
 })
 
@@ -32,3 +33,21 @@ function renderButtons() {
 
     }
   }
+
+  //function for showing the weather information on the page
+  function showWeather(cityname) {
+    var api = "515798d11075abbf042d6d0ba0edef46";
+    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid="+api;
+    $.ajax({
+        url: queryUrl,
+        type: "Get"
+      }).then(function(response){
+        console.log(response);
+      })
+  }
+
+//event listener for buttons already created
+$(document).on("click", ".city-btn", function() {
+    var cityname = $(this).attr("data-name");
+    showWeather(cityname)
+});
