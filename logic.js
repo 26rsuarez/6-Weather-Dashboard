95764b1ab297675a593d4b740cfb6d98f88cfcb0
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-10-05 10:25:41 -0500

    added api calls for the uv index and 5day forecast

--- logic.js
+++ logic.js
@@ -8,6 +8,7 @@ $("#searchbtn").on("click", function(event){
     cityArr.push(city);
     renderButtons();
     showWeather(city);
+    fiveDayForecast(city);
 
 })
 
@@ -34,20 +35,55 @@ function renderButtons() {
     }
   }
 
-  //function for showing the weather information on the page
-  function showWeather(cityname) {
+//function for showing the weather information on the page
+function showWeather(cityname) {
+  var api = "515798d11075abbf042d6d0ba0edef46";
+  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid="+api;
+  var lat;
+  var lon;
+  $.ajax({
+      url: queryUrl,
+      type: "Get"
+    }).then(function(response){
+      console.log(response);
+      var temperature = response.main.temp;
+      var humidity = response.main.humidity;
+      var windSpeed = response.wind.speed;
+      lat = response.coord.lat;
+      lon = response.coord.lon;
+      // var uvIndex = response.data[0].value;
+      $("#nameofcity").text(cityname);
+      $("#temperature").text(temperature);
+      $("#humidity").text(humidity);
+      $("#windspeed").text(windSpeed);
+      // $("#uvindex").text(uvIndex);
+    })
+  url = "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+api;
+  $.ajax({
+    url: uvIndexURL,
+    type: "Get"
+  }).then(function(response){
+    console.log(response);
+  })
+}
+
+function fiveDayForecast(cityname) {
     var api = "515798d11075abbf042d6d0ba0edef46";
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+cityname+"&appid="+api;
+    var queryUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+cityname+"&appid="+api;
     $.ajax({
         url: queryUrl,
         type: "Get"
       }).then(function(response){
         console.log(response);
+        // for (var i=0; i<40; i+=8) {
+
+        // }
       })
-  }
+}
 
 //event listener for buttons already created
 $(document).on("click", ".city-btn", function() {
     var cityname = $(this).attr("data-name");
-    showWeather(cityname)
+    showWeather(cityname);
+    fiveDayForecast(cityname);
 });
