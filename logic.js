ca766de1963f340fe7740b43593bfc452d1d9fe6
Raunel Suarez-Magana <rsuarez_26@utexas.edu>
2020-10-05 23:37:33 -0500

    added icons and styling to buttons

--- logic.js
+++ logic.js
@@ -23,15 +23,13 @@ function renderButtons() {
 
       // Then dynamicaly generating buttons for each movie in the array
       var a = $("<button>");
-      a.addClass("city-btn");
+      a.addClass("city-btn btn btn-outline-secondary btn-block");
       // Adding a data-attribute
       a.attr("data-name", cityArr[i]);
       // Providing the text
       a.text(cityArr[i]);
       // Adding the button to the buttons-area div
       $("#buttons-area").append(a);
-      $("#buttons-area").append($("<br>"));
-
     }
   }
 
@@ -50,11 +48,18 @@ function showWeather(cityname) {
       var temperatureF = (temperature-273.15)*9/5+32;
       var humidity = response.main.humidity;
       var windSpeed = response.wind.speed*2.2;
+      var iconTag = $("<img>").attr("src","https://openweathermap.org/img/wn/" +response.weather[0].icon+ "@2x.png");
       // lat and lon are neeeded to call for uv index
       var lat = response.coord.lat;
       var lon = response.coord.lon;
+
+      var dateNeeded = new Date();
+      var day = dateNeeded.getDate();
+      var month = dateNeeded.getMonth();
+      var year = dateNeeded.getFullYear();
       
-      $("#nameofcity").text(cityname);
+      $("#nameofcity").text(cityname+"("+month+"/"+day+"/"+year+")");
+      $("#nameofcity").append(iconTag);
       $("#temperature").text("Temperature: "+temperatureF.toFixed(1)+"°F");
       $("#humidity").text("Humidity: "+humidity+"%");
       $("#windspeed").text("Wind Speed: "+windSpeed.toFixed(1)+" MPH");
@@ -65,21 +70,22 @@ function showWeather(cityname) {
         url: uvIndexURL,
         type: "Get"
       }).then(function(response){
-        console.log(response);
+        
         //the uv index will be displayed on the page
         var uvIndex = response.value;
         $("#uvindex").text("UV index: ");
+        console.log(uvIndex);
         //the uv will be color coded green, yellow, or red depending on how severe it is
         if (uvIndex<3) {
           var uvValue = $("<span>").attr("class", "badge badge-success").text(uvIndex);
           $("#uvindex").append(uvValue);
         }
-        else if (uvIndex>=3 || uvIndex<6) {
-          var uvValue = $("<span>").attr("class", "badge badge-danger").text(uvIndex);
+        else if (uvIndex>=3 && uvIndex<6) {
+          var uvValue = $("<span>").attr("class", "badge badge-warning").text(uvIndex);
           $("#uvindex").append(uvValue);
         }
         else {
-          var uvValue = $("<span>").attr("class", "badge badge-warning").text(uvIndex);
+          var uvValue = $("<span>").attr("class", "badge badge-danger").text(uvIndex);
           $("#uvindex").append(uvValue);
         }
       })
@@ -95,12 +101,14 @@ function fiveDayForecast(cityname) {
         url: queryUrl,
         type: "Get"
       }).then(function(response){
-        console.log(response);
         $("#5daytitle").text("5-Day Forecast:");
         
 
         // the object returned has 40 arrays with the day changing every 8 arrays
         for (var i=0; i<40; i+=8) {
+          //clear the div text first
+          var divId = "#Forecast" + i;
+          $(divId).empty();
           //the date is in the object but it needs to be parsed
           var defaultDate = response.list[i].dt_txt;
           var dateNeeded = defaultDate.split(" ");
@@ -115,11 +123,13 @@ function fiveDayForecast(cityname) {
           var hum = response.list[i].main.humidity;
 
           // then the important properties are added to the html
-          var divId = "#Forecast" + i;
           var date = $("<p>").text(+month+"/"+day+"/"+year);
+          //the icon can be obtained from the object and the website
+          var icon = $("<img>").attr("src","https://openweathermap.org/img/wn/" +response.list[i].weather[0].icon+ "@2x.png");
+
           var temp = $("<p>").text("Temp: "+temperatureF.toFixed(1)+"°F");
           var humidity = $("<p>").text("Humidity: "+hum+"%");
-          $(divId).append(date,temp,humidity);
+          $(divId).append(date,icon,temp,humidity);
         }
       })
 }
